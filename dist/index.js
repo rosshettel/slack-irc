f517ec6a7af13e8900c4d30ac3ee0a9446f58df7
Ross Hettel <ross@het.tel>
2016-09-27 13:46:07 -0500

    add dist directory so we can npm install with github url

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _winston = require('winston');
+
+var _winston2 = _interopRequireDefault(_winston);
+
+var _helpers = require('./helpers');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* istanbul ignore next */
+
+/* eslint-disable import/newline-after-import */
+if (process.env.NODE_ENV === 'development') {
+  _winston2.default.level = 'debug';
+}
+
+/* istanbul ignore next */
+if (!module.parent) {
+  var _require = require('./cli');
+
+  var cli = _require.default;
+
+  cli();
+}
+
+exports.default = _helpers.createBots;
